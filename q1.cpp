39d22fe4679e74617f7ec7ed24a22391cff08e76
rarathore02 <rashikarathore.rr02@gmail.com>
2025-12-01 22:24:04 +0530

    Update q1.cpp

--- q1.cpp
+++ q1.cpp
@@ -18,3 +18,22 @@ void BFS(int start, vector<vector<int>>& adj, int n){
         for(int x : adj[node]){
             if(!visited[x]){
                 visited[x] = true;
+                q.push(x);
+            }
+        }
+    }
+}
+
+int main(){
+    int n = 5;
+    vector<vector<int>> adj(n);
+
+    adj[0] = {1,2};
+    adj[1] = {0,3};
+    adj[2] = {0,3};
+    adj[3] = {1,2,4};
+    adj[4] = {3};
+
+    cout << "BFS starting from node 0: ";
+    BFS(0, adj, n);
+}
